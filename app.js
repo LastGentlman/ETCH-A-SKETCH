cdfb74f2bf5b0af4b66270b247b1ee4c05fff2cb
Rodrigo <gokusan@keemail.me>
2022-03-13 17:14:33 -0600

    - UI menu HTML - Some UI Styling in CSS - Some variables declared in JS

---
+++ app.js
@@ -0,0 +1,16 @@
+const square = document.createElement('div');
+square.className = 'square'
+document.querySelector('.divs-box').appendChild(square);
+
+const colorPicker = document.querySelector('.color-picker');
+const colorBtn = document.querySelector('.color-btn');
+const colourfulBtn = document.querySelector('.colourful-btn');
+const eraserBtn = document.querySelector('.eraser-btn');
+const clearBtn = document.querySelector('.clear-btn');
+const labelRange = document.querySelector('.label-range')
+const sizeRange = document.querySelector('.size-range');
+
+
+function squareGenerator(a,b) {
+
+}
