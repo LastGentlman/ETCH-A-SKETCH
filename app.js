5d6f3d94bbf9d5c02060bf73586bdafc5d8ce99c
Rodrigo <gokusan@keemail.me>
2022-06-27 14:26:23 -0500

    Fully functional     clearBoard = Fixed     flag feature adedd successfully

--- app.js
+++ app.js
@@ -1,16 +1,66 @@
-const square = document.createElement('div');
-square.className = 'square'
-document.querySelector('.divs-box').appendChild(square);
-
 const colorPicker = document.querySelector('.color-picker');
-const colorBtn = document.querySelector('.color-btn');
+// const colorBtn = document.querySelector('.color-btn');
 const colourfulBtn = document.querySelector('.colourful-btn');
 const eraserBtn = document.querySelector('.eraser-btn');
 const clearBtn = document.querySelector('.clear-btn');
-const labelRange = document.querySelector('.label-range')
+const labelRange = document.querySelector('.label-range');
 const sizeRange = document.querySelector('.size-range');
+const board = document.querySelector('.divs-box');
+const cssRoot = document.querySelector(':root'); //to bring the css variables
+const cssVars = getComputedStyle(cssRoot);
+const flagE = document.querySelector('.flag')
 
+let flag = true;
+let color = '';
 
-function squareGenerator(a,b) {
+function pincelColor() {
+    if (flag) {
+    switch (color) {
+        case 'picker':
+            this.style.backgroundColor = colorPicker.value;
+            break;
+        case 'eraser':
+            let squareBg = cssVars.getPropertyValue('--square-bg')
+            this.style.backgroundColor = squareBg;
+            break;
+        case 'colorful':
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }}
+}
 
+function colorIs(v) {
+    color = v;
+}
+
+function toggleFlag() {
+    if (!flag) flagE.style.backgroundColor = 'red'
+    flag = !flag;
+    if (flag && color !== '') flagE.style.backgroundColor = 'green'
+    else flagE.style.backgroundColor = 'red'
+}
+board.addEventListener('click', toggleFlag)
+
+function squareGenerator(size) {
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    let total = size * size;
+
+    for (let i = 0; i < total; i++) {
+        const square = document.createElement('div');
+        square.className = 'square';
+        document.querySelector('.divs-box').appendChild(square);
+        square.addEventListener('mouseover', pincelColor);
+    }
+}
+resize(sizeRange.value)
+
+function resize(v) {
+    squareGenerator(v);
+    labelRange.textContent = `${v} x ${v}`;
+}
+
+function clearBoard() {
+    board.innerHTML = ''
+    resize(sizeRange.value);
 }
